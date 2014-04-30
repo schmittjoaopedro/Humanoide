7b1bf829327ac4050072175d22f6b93a3c2e631a
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2014-04-29 21:15:06 -0300

    giroscopio

--- Sensor_Giroscopio/Sensor_Giroscopio.ino
+++ Sensor_Giroscopio/Sensor_Giroscopio.ino
@@ -3,6 +3,8 @@
 
 L3G gyro;
 
+int rate = 20000;
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
@@ -12,20 +14,34 @@ void setup() {
     Serial.println("Failed to autodetect gyro type!");
     while (1);
   }
-
+  pinMode(50, OUTPUT);
   gyro.enableDefault();
 }
 
 void loop() {
   gyro.read();
 
-  Serial.print("G ");
-  Serial.print("X: ");
-  Serial.print((int)gyro.g.x);
-  Serial.print(" Y: ");
-  Serial.print((int)gyro.g.y);
-  Serial.print(" Z: ");
-  Serial.println((int)gyro.g.z);
+  int x = (int)gyro.g.x;
+  int z = (int)gyro.g.z;
+  int y = (int)gyro.g.y; 
 
-  delay(100);
+  Serial.print("G ");
+  Serial.print("\t\tX: ");
+  Serial.print(x);
+  Serial.print("\t\tY: ");
+  Serial.print(y);
+  Serial.print("\t\tZ: ");
+  Serial.println(z);
+
+  if(
+    x > rate || x < -rate ||
+    z > rate || z < -rate || 
+    y > rate || y < -rate
+  ) {
+    digitalWrite(50,HIGH);
+    delay(1000);  
+  } else {
+    digitalWrite(50,LOW);    
+  }
+
 }
